6c6d8c8c4aba5b91f606467d4ec06ca01a9efd6b
Anis KADRI <k.anis@outlook.com>
2022-10-25 18:50:14 +0200

    "Fichiers et dossiers"

---
+++ excercicesJava/src/JouerAvecDesFichiers/ParcoursRecuFichiersetDossiers.java
@@ -0,0 +1,32 @@
+package JouerAvecDesFichiers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ParcoursRecuFichiersetDossiers {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\KADRI\\Desktop\\UGA\\NET");
+        try {
+
+            Stream<Path> stream1 = Files.walk(Paths.get(String.valueOf(path)));
+            stream1.filter(Files ::isDirectory).forEach(System.out::println);
+
+            System.out.println("--------------------------------------------------------");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("--------------------------------------------------------");
+
+
+            Stream<Path> stream2 = Files.walk(Paths.get(String.valueOf(path)));
+            stream2.filter(Files::isRegularFile).forEach(System.out::println);
+        }finally {
+
+        }
+    }
+}
