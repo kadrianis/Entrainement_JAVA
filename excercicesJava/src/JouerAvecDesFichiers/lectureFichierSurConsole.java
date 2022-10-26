9070c0d8921bd4e180839b77acf1a45250204823
Anis KADRI <k.anis@outlook.com>
2022-10-26 21:03:07 +0200

    "jouer avec des flux"

---
+++ excercicesJava/src/JouerAvecDesFichiers/lectureFichierSurConsole.java
@@ -0,0 +1,22 @@
+package JouerAvecDesFichiers;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+
+public class lectureFichierSurConsole {
+    public static void main(String[] args) throws IOException {
+        Path f1 = Paths.get("C:\\Users\\KADRI\\Desktop\\a.txt");
+        try (Reader in = Files.newBufferedReader(f1, Charset.defaultCharset())) {
+            int c;
+            String res ="";
+            while ((c = in.read()) != -1) {
+               res+=(char)c;
+            }
+            System.out.println(res);
+        } catch (NoSuchFileException e) {
+            System.err.println("le fichier " + f1 + " n'existe pas");
+        }
+    }
+}
+
---
+++ excercicesJava/src/JouerAvecDesFichiers/simpleTestSurFlux.java
@@ -0,0 +1,46 @@
+package JouerAvecDesFichiers;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class simpleTestSurFlux {
+
+    public simpleTestSurFlux() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path p = Paths.get("src/fr/uga/miashs/dciss/cours1live/trucprogramme.java");
+        System.out.println(" je suis ou je ne suis pas  "+ p.getClass());
+        System.out.println(" je suis ou je ne suis pas  "+ p.getFileName());
+
+        for (int i =0; i<p.getNameCount();i++) {
+            System.out.println(p.getName(i));
+        }
+
+        System.out.println((p.getName(p.getNameCount()-1)));
+        System.out.println(p.getParent());
+        System.out.println(p.toAbsolutePath());
+        System.out.println(p.isAbsolute());
+
+        Path p2 = Paths.get("anis/truc");
+
+        System.out.println(p2.isAbsolute());
+        System.out.println(p2.toAbsolutePath());
+        System.out.println(p2.toAbsolutePath().getRoot());
+
+        Path abs = Paths.get("/chose");
+        Path nv = abs.resolve(p);
+        System.out.println(nv.normalize());
+        try {
+            Path nv2 = Paths.get("../../src/fr/uga/miashs/dciss/cours1live/trucprogramme.java").toRealPath();
+        } catch (IOException e) {
+            System.out.println(" un probleme, je le gere" + nv + " n existe pas");
+        }
+        System.out.println("coucou");
+        for (int i=0; i<args.length; i++) {
+            System.out.println("arg " +i+ " "+ args[i]);
+        }
+
+    }
+
+}
